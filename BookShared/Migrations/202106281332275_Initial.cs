56df01a78dd85e1dd6cf6761c28cb6bbbd9af75f
damien5215 <37832934+damien5215@users.noreply.github.com>
2021-06-28 22:55:08 +0100

    Add Migrations and Deployed Site

---
+++ BookShared/Migrations/202106281332275_Initial.cs
@@ -0,0 +1,104 @@
+﻿namespace BookShared.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Authors",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 100),
+                        DOB = c.String(nullable: false, maxLength: 100),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.BookGenres",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        BookId = c.Int(nullable: false),
+                        GenreId = c.Int(nullable: false),
+                        FictionId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Books", t => t.BookId, cascadeDelete: true)
+                .ForeignKey("dbo.Fictions", t => t.FictionId, cascadeDelete: true)
+                .ForeignKey("dbo.Genres", t => t.GenreId, cascadeDelete: true)
+                .Index(t => t.BookId)
+                .Index(t => t.GenreId)
+                .Index(t => t.FictionId);
+            
+            CreateTable(
+                "dbo.Books",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        AuthorId = c.Int(nullable: false),
+                        Title = c.String(),
+                        Description = c.String(),
+                        Img = c.String(),
+                        Price = c.Decimal(nullable: false, precision: 18, scale: 2),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Authors", t => t.AuthorId, cascadeDelete: true)
+                .Index(t => t.AuthorId);
+            
+            CreateTable(
+                "dbo.Fictions",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Genres",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        GenreOfBook = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Carts",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        BookId = c.Int(nullable: false),
+                        Quantity = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Books", t => t.BookId, cascadeDelete: true)
+                .Index(t => t.BookId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Carts", "BookId", "dbo.Books");
+            DropForeignKey("dbo.BookGenres", "GenreId", "dbo.Genres");
+            DropForeignKey("dbo.BookGenres", "FictionId", "dbo.Fictions");
+            DropForeignKey("dbo.BookGenres", "BookId", "dbo.Books");
+            DropForeignKey("dbo.Books", "AuthorId", "dbo.Authors");
+            DropIndex("dbo.Carts", new[] { "BookId" });
+            DropIndex("dbo.Books", new[] { "AuthorId" });
+            DropIndex("dbo.BookGenres", new[] { "FictionId" });
+            DropIndex("dbo.BookGenres", new[] { "GenreId" });
+            DropIndex("dbo.BookGenres", new[] { "BookId" });
+            DropTable("dbo.Carts");
+            DropTable("dbo.Genres");
+            DropTable("dbo.Fictions");
+            DropTable("dbo.Books");
+            DropTable("dbo.BookGenres");
+            DropTable("dbo.Authors");
+        }
+    }
+}
---
+++ BookWebApp/obj/Release/AspnetCompileMerge/TempBuildDir/Scripts/products.js
@@ -0,0 +1,31 @@
+﻿$(document).ready(function () {
+
+    //var bla = $('#txt_name').val("Tuna goes here!");
+    //$('#tuna').val("Tuna goes here!");
+    //$('#tuna').text('test');
+    console.log("Page Loaded!");
+
+    //$('.button_action').click(function (index){
+    //    $('#tuna').text('test');
+    //    console.log("Tuna Set!");
+    //})
+
+    $('.testClass').click(function (index) {
+        console.log("Button Clicked!");
+
+        $.ajax({
+            url: this.href,
+            contentType: 'application/html; charset=utf-8',
+            type: 'GET',
+            success: function (result) {
+
+                //$('#showEditProd').html(result);
+                //$("#cartDiv").load();
+                $("#cartDiv").load("https://localhost:44307/Cart/_Cart");
+                console.log("AJAX Sucess!");
+            }
+        });
+        return false;
+    });
+
+})
